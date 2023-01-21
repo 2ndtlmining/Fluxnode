55db3aaada21b221746afc776c3ead54233b6d50
andy97 <hiroshi041197@gmail.com>
2023-01-21 12:27:42 +0700

    [Update] Use `CodeBlock.Full`

--- client/src/guides/GuidesView.jsx
+++ client/src/guides/GuidesView.jsx
@@ -483,7 +483,7 @@ sudo systemctl restart zelcash
         <Accordion.Item eventKey='12'>
           <Accordion.Header>{this.renderBullet(25)} Troubleshoot with Fluxnode viewer created by Car </Accordion.Header>
           <Accordion.Body>
-            {CodeBlock.Inline(`
+            {CodeBlock.Full(`
 bash -i <(curl -s 
 https://raw.githubusercontent.com/JKTUNING/Flux-Node-Tools/main/flux_node_viewer.sh)
 `)}
