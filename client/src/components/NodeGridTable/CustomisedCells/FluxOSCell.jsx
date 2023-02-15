5226cc542a09b0fd6fd5afa74878e0060cb14fa6
Victor Lam <quanphuoc89@gmail.com>
2023-02-15 22:05:45 +1100

    add missing code

--- client/src/components/NodeGridTable/CustomisedCells/FluxOSCell.jsx
+++ client/src/components/NodeGridTable/CustomisedCells/FluxOSCell.jsx
@@ -1,13 +1,9 @@
 import { fluxos_version_string, fv_compare } from 'main/flux_version';
 export const FluxOSCell = (props) => {
-    const fluxos = props?.value;
-    const gstore = props?.data?.gstore;
+  const fluxos = props?.data?.flux_os;
+  const gstore = props?.data?.gstore;
 
-    const is_fluxos_outdated = fluxos && gstore ? fv_compare(fluxos, gstore?.fluxos_latest_version) == -1 : true;
+  const is_fluxos_outdated = fluxos && gstore ? fv_compare(fluxos, gstore?.fluxos_latest_version) == -1 : true;
 
-    return (
-        <span className={is_fluxos_outdated ? 'fw-bolder outdated-flux-ver' : ''}>
-            {fluxos_version_string(fluxos)}
-        </span>
-    );
+  return <span className={is_fluxos_outdated ? 'fw-bolder outdated-flux-ver' : ''}>{props?.value}</span>;
 };
